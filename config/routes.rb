2b2e501db347857b5730f7e8b35b38e9ec4135c6
Sujit Jagdale <sujit.jagdale@gmail.com>
2023-02-23 23:55:21 +0000

    Adding navbar styling, layout and dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'dashboards#index'
+  resources 'dashboards', only: :index
 end
